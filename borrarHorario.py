52565937d763eef5454b1f074eda810b2bda4c75
kevinPomna <kepomacu@uide.edu.ec>
2023-02-08 09:18:31 -0500

    database

---
+++ borrarHorario.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+cliente = MongoClient('localhost:27017')
+db = horaio.db_tienda_informatica
+def delete():
+    try:
+    
+        criterio = input("\n Ingresa el id del registro a eliminar \n ")
+        db.cliente.delete_many(
+            {'id':float(criterio)}
+        )
+        print("Registro",criterio,"Eliminado correctamente")
+        print(f"Registro{criterio} Eliminado correctamente")
+
+    except ImportError:
+         platform_specific_module = None
+         print(str(e))
---
+++ borrarVisita.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+cliente = MongoClient('localhost:27017')
+db = visita.db_tienda_informatica
+def delete():
+    try:
+    
+        criterio = input("\n Ingresa el id del registro a eliminar \n ")
+        db.cliente.delete_many(
+            {'id':float(criterio)}
+        )
+        print("Registro",criterio,"Eliminado correctamente")
+        print(f"Registro{criterio} Eliminado correctamente")
+
+    except ImportError:
+         platform_specific_module = None
+         print(str(e))
+         
